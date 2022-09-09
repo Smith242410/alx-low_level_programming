c4171a531c0d668f2a7246c347731f984a2aa7c4
Smith Anorue <anorueugochukwu55@gmail.com>
2022-09-09 15:34:35 -0700

    Prayers

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+  /**
+   *main - Entry point
+   *
+   * Return: Always 0
+   */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
