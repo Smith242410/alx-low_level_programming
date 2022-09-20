990ea88ae2b5f9600c7fdcdfa9385619794a6b91
Smith Anorue <anorueugochukwu55@gmail.com>
2022-09-19 21:36:51 -0700

    Prayers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: A value
+ */
+int main(void);
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,12 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *b;
+	*b = *a;
+	*a = x;
+}
