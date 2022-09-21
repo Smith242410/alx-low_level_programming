0885f12b5cf65f494d5498799866bab15c7f208b
Smith Anorue <anorueugochukwu55@gmail.com>
2022-09-21 14:46:40 -0700

    Prayers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+char *_strncat(char *dest, char *src, int n);
+/**
+ * main - Function to print a partialy concatenated string
+ *
+ * Return: Always 0
+ */
+int main(void);
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,18 +3,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
-	char *p;
+	return (strcat(dest, src));
+}
 
-	while(*(dest + i) != '\0')
-		i++;
-	while (*(src + j) != '\0')
-	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
-	}
-	*(dest + i) = '\0';
-	p = dest;
-	return (p);
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
 }
