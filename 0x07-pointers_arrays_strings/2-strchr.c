19d7e11722dc92ab5f4c43980ea689ac0b1a57c0
Smith Anorue <anorueugochukwu55@gmail.com>
2022-09-26 06:06:38 -0700

    Prayers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+char *_strchr(char *s, char c);
+/**
+ * main - Entyr point
+ *
+ * Return: Always 0
+ */
+int main(void);
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (s);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
+
+char *_strchr(char *s, char c)
+{
+	char *x;
+
+	x = strchr(s, c);
+	return (x);
+}
